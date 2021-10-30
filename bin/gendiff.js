bca9cdfbee39965ffbedc5946c7f43379dc82d01
Alisher Kim <cerberstrix@gmail.com>
2021-10-30 22:27:21 +0500

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,10 @@ import { program } from "commander";
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .argument('<filepath1>, <filepath2>')
+  .option('-p, --pizza-type <type>', 'flavour of pizza')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
+  
 
 program.parse();
