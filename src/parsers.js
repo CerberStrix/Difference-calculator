83c5c2c1dc47c9aa5fedec11fcbfe976e6b04c54
Alisher Kim <cerberstrix@gmail.com>
2021-11-30 16:30:21 +0500

    fix parsers func

--- src/parsers.js
+++ src/parsers.js
@@ -2,19 +2,11 @@ import yaml from 'js-yaml';
 import path from 'path';
 
 const getParser = (pathOfFile) => {
-  let parse;
   const format = path.extname(pathOfFile);
-
-  if (format === '') {
-    parse = JSON.parse;
-  } else if (format === '.yaml') {
-    parse = yaml.load;
-  } else if (format === '.yml') {
-    parse = yaml.load;
-  } else if (format === '.json') {
-    parse = JSON.parse;
+  if (format === '.yaml' || format === '.yml') {
+    return yaml.load;
   }
-  return parse;
+  return JSON.parse;
 };
 
 export default getParser;
