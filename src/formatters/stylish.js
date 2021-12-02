be80598855fb69e2b1b3e0e1574f97a6c47f8665
Alisher Kim <cerberstrix@gmail.com>
2021-12-03 04:22:49 +0500

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,50 +1,22 @@
-const replacer = ' ';
-const spacesCount = 4;
-
-const stringlify = (node, depth = 1) => {
-  if (typeof node !== 'object') {
-    return node.toString();
-  }
-  if (node === null) {
-    return 'null';
-  }
-  const indentSize = depth * spacesCount;
-  const currentIndent = replacer.repeat(indentSize);
-  const bracketIndent = replacer.repeat(indentSize - spacesCount);
-  const lines = Object
-    .entries(node)
-    .map(([key, val]) => `${currentIndent}${key}: ${stringlify(val, depth + 1)}`);
-
-  return [
-    '{',
-    ...lines,
-    `${bracketIndent}}`,
-  ].join('\n');
-};
+import { getIndent, stringlify } from '../utils.js';
 
 const getStylish = (value) => {
   const iter = (currentValue, depth) => {
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const indendForAdded = `${replacer.repeat(indentSize - 2)}+ `;
-    const indendForRemove = `${replacer.repeat(indentSize - 2)}- `;
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-
     const lines = currentValue.map((item) => {
       const {
         type, key, children, val, val1, val2,
       } = item;
       switch (type) {
         case 'recursion':
-          return `${currentIndent}${key}: ${iter(children, depth + 1)}`;
+          return `${getIndent(depth, type)}${key}: ${iter(children, depth + 1)}`;
         case 'added':
-          return `${indendForAdded}${key}: ${stringlify(val, depth + 1)}`;
+          return `${getIndent(depth, type)}${key}: ${stringlify(val, depth + 1)}`;
         case 'removed':
-          return `${indendForRemove}${key}: ${stringlify(val, depth + 1)}`;
+          return `${getIndent(depth, type)}${key}: ${stringlify(val, depth + 1)}`;
         case 'same':
-          return `${currentIndent}${key}: ${stringlify(val, depth + 1)}`;
+          return `${getIndent(depth, type)}${key}: ${stringlify(val, depth + 1)}`;
         case 'updated':
-          return `${indendForRemove}${key}: ${stringlify(val1, depth + 1)}\n${indendForAdded}${key}: ${stringlify(val2, depth + 1)}`;
+          return `${getIndent(depth, 'removed')}${key}: ${stringlify(val1, depth + 1)}\n${getIndent(depth, 'added')}${key}: ${stringlify(val2, depth + 1)}`;
         default:
           throw new Error(`Unknown type: '${type}'!`);
       }
@@ -53,7 +25,7 @@ const getStylish = (value) => {
     return [
       '{',
       ...lines,
-      `${bracketIndent}}`,
+      `${getIndent(depth, 'bracket')}}`,
     ].join('\n');
   };
 
--- src/utils.js
+++ src/utils.js
@@ -7,4 +7,40 @@ const getNormalizePath = (filepath) => {
 
 const getTypeOfFile = (pathOfFile) => path.extname(pathOfFile);
 
-export { getNormalizePath, getTypeOfFile };
+const getIndent = (depth, type = 'none') => {
+  const replacer = ' ';
+  const spacesCount = 4;
+  const indentSize = depth * spacesCount;
+  switch (type) {
+    case 'added':
+      return `${replacer.repeat(indentSize - 2)}+ `;
+    case 'removed':
+      return `${replacer.repeat(indentSize - 2)}- `;
+    case 'bracket':
+      return replacer.repeat(indentSize - spacesCount);
+    default:
+      return replacer.repeat(indentSize);
+  }
+};
+
+const stringlify = (node, depth = 1) => {
+  if (typeof node !== 'object') {
+    return node.toString();
+  }
+  if (node === null) {
+    return 'null';
+  }
+  const lines = Object
+    .entries(node)
+    .map(([key, val]) => `${getIndent(depth)}${key}: ${stringlify(val, depth + 1)}`);
+
+  return [
+    '{',
+    ...lines,
+    `${getIndent(depth, 'bracket')}}`,
+  ].join('\n');
+};
+
+export {
+  getNormalizePath, getTypeOfFile, getIndent, stringlify,
+};
